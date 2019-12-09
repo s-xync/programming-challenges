9ceda5470d441cee5d041b4da9678c025ed93917
SaiKumar Immadi <immadisaikumar@ymail.com>
2019-12-09 12:58:33 +0530

    adds problem statement

---
+++ array-intersection/index.js
@@ -0,0 +1,11 @@
+/*
+Write a function, FindIntersection, that reads an array of strings which will contain two elements: the first element will represent a list of comma-separated numbers sorted in ascending order, the second element will represent a second list of comma-separated numbers (also sorted). Your goal is to return a string of numbers that occur in both elements of the input array in sorted order. If there is no intersection, return the string "false".
+
+For example: if the input array is ["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"] the output string should be "1, 4, 13" because those numbers appear in both strings. The array given will not be empty, and each string inside the array will be of numbers sorted in ascending order and may contain negative numbers.
+*/
+
+const array = ["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"];
+
+const main = () => {};
+
+main();
