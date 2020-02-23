57e7997f9d041d38aa8e923490db41036ec98d01
SaiKumar Immadi <immadisaikumar@ymail.com>
2020-02-23 14:17:57 +0530

    second method for the find number of ways to calculate cents

--- problems/find-number-of-ways-cents.js
+++ problems/find-number-of-ways-cents.js
@@ -2,29 +2,40 @@
 Given an infinite amount of quarters, dimes, nickels, and pennies, write a function that returns the number of ways of representing n cents with coins.
 */
 
-const availableCoins = [25, 10, 5, 1];
+const availableCoins = [1, 5, 10, 25];
 // const availableCoins = [1, 2, 3];
 
-function count(length, total) {
-  if (total === 0) {
-    return 1;
-  }
+function count1(length, total) {
+  if (total === 0) return 1;
 
-  if (total < 0) {
-    return 0;
-  }
+  if (total < 0) return 0;
 
-  if (length <= 0 && total > 0) {
-    return 0;
-  }
+  if (length <= 0 && total > 0) return 0;
 
   return (
-    count(length - 1, total) + count(length, total - availableCoins[length - 1])
+    count1(length - 1, total) +
+    count1(length, total - availableCoins[length - 1])
   );
 }
 
+function count2(total, index) {
+  if (total === 0) return 1;
+
+  if (total < 0) return 0;
+
+  let numberOfWays = 0;
+  for (let i = index; i < availableCoins.length; i += 1) {
+    numberOfWays += count2(total - availableCoins[i], i);
+  }
+  return numberOfWays;
+}
+
 function main(total) {
-  console.log(count(availableCoins.length, total));
+  console.log({
+    total,
+    count1: count1(availableCoins.length, total),
+    count2: count2(total, 0)
+  });
 }
 
 main(0);
